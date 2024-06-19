27b7729241aa0af5c03ff95d0198567f72f0c022
Akhil-Patil-Bagili <akhilpatilbagili@gmail.com>
2024-06-19 00:31:03 -0700

    added favicon and title

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,18 @@
 // pages/_app.tsx
 import '../globals.css'; // Adjust the path if necessary
 import type { AppProps } from 'next/app';
+import Head from 'next/head';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <title>APatil Portfolio</title>
+        <link rel="icon" href="/favicon.png" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
